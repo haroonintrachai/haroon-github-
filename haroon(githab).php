2a4f3c2ad5f323d751c37416dd6add28d8db120f
haroonintrachai <haroon.poompradit@intrachai.ac.th>
2023-02-23 08:53:06 +0700

    v1

---
+++ haroon(githab).php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> นาย ฮารูน พุ่มประดิษฐ์ </h1>
+    <h1> รหัสนักศึกษา  64309010012 </h1>
+    <h1> เลขที่ 02 </h1>
+</body>
+</html>
